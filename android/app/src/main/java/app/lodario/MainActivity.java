aff9f67c68428268e384e0c92f9ccabe46afdbd3
Prolaesio <n.dawg9.12@gmail.com>
2026-09-24 12:22:11 +0100

    Configure production Android app

--- android/app/src/main/java/app/lodario/MainActivity.java
+++ android/app/src/main/java/app/lodario/MainActivity.java
@@ -1,4 +1,4 @@
-package com.example.app;
+package app.lodario;
 
 import com.getcapacitor.BridgeActivity;
 
--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,9 +1,14 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
-  appName: 'lodario',
-  webDir: 'public'
+  appId: 'app.lodario',
+  appName: 'Lodario',
+  webDir: 'native-shell',
+  server: {
+    url: 'https://lodario.vercel.app',
+    cleartext: false,
+    errorPath: 'offline.html'
+  }
 };
 
 export default config;
